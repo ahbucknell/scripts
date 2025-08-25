393fcf3a6ea77c45895f4fe4ac860911ff061b93
ahbucknell <128325035+ahbucknell@users.noreply.github.com>
2025-08-25 21:13:29 +0100

    Initial

---
+++ alphaMover.jl
@@ -0,0 +1,41 @@
+using JSON, ArgParse
+
+function parseArgs()
+    settings = ArgParseSettings()
+    @add_arg_table settings begin
+        "arg1"
+            help = "position of AlphaFold output directory"
+            required = true
+            arg_type = String
+    end
+    return parse_args(settings)
+end
+
+function findBestPkl(path)
+    pklContents = JSON.parsefile(joinpath(path, "ranking_debug.json"))
+    topPikl = pklContents["order"][1]
+    return topPikl * ".pkl"
+end
+
+function main()
+    args = parseArgs()
+    targetDir = joinpath(@__DIR__, args["arg1"])
+    dirContents = readdir(targetDir)
+    notHidden = filter(f -> !startswith(basename(f), "."), dirContents)
+    for subDir in joinpath.(targetDir, notHidden)
+        targetPikl = findBestPkl(subDir)
+        fileName = split(subDir, "/")[end]
+        for file in [["ranked_0.pdb", ".pdb"], [targetPikl, ".pkl"]]
+            old = joinpath(subDir, file[1])
+            new = joinpath(dirname(subDir), fileName * file[2])
+            mv(old, new)
+        end
+    end
+end
+
+main()
+
+
+
+
+
