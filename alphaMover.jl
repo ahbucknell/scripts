465f08f9af5433c392bb5b131bfacb6523c6f7eb
ahbucknell <128325035+ahbucknell@users.noreply.github.com>
2025-08-26 10:26:54 +0100

    Removed unnecessary fn()

--- alphaMover.jl
+++ alphaMover.jl
@@ -1,5 +1,4 @@
 using JSON, ArgParse
-
 function parseArgs()
     settings = ArgParseSettings()
     @add_arg_table settings begin
@@ -10,14 +9,10 @@ function parseArgs()
     end
     return parse_args(settings)
 end
-
 function findBestPkl(path)
     pklContents = JSON.parsefile(joinpath(path, "ranking_debug.json"))
     topPikl = pklContents["order"][1]
     return "result_" * topPikl * ".pkl"
-end
-
-function mvTargetFiles(subdir)
 end
 
 function main()
@@ -39,7 +34,6 @@ function main()
         end
     end
 end
-
 main()
 
 
